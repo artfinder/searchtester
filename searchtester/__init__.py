c9448400e9418b3770cff913f4edf6dddda97129
James Aylett <james@tartarus.org>
2011-06-14 14:29:39 +0100

    Better docs.

--- searchtester/__init__.py
+++ searchtester/__init__.py
@@ -6,7 +6,7 @@ def runtest():
     
     parser = OptionParser(
         usage="usage: %prog [options] input output",
-        description="Given a file of searches and URLs (absolute, or relative to the search HTTP endpoint) that should be returned in order of importance, outputs a file indicating the 0-indexed position each URL was found on the first page, plus an 'accuracy' score; prints out the query and accuracy score as it goes. Prints a final summary including the average accuracy score. Errors in testing a query (eg HTTP failures) are treated as all expected matches being missing.",
+        description="Given a TSV file of searches and URLs (absolute, or relative to the search HTTP endpoint) that should be returned in order of importance, outputs a TSV file indicating the 0-indexed position each URL was found on the first page, plus an 'accuracy' score; prints out the query and accuracy score as it goes. Prints a final summary including the average accuracy score. Errors in testing a query (eg HTTP failures) are treated as all expected matches being missing.",
     )
     parser.add_option("-s", "--selector", dest="selector", help="CSS selector used to find matches", default="ol li a")
     parser.add_option("-e", "--endpoint", dest="endpoint", help="Search HTTP endpoint to test", default="http://localhost:8000/search/")
